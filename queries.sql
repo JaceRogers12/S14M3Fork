c308f6a158bd669bba7c511610eb878367aa95ec
JaceRogers12 <jacerogers12@gmail.com>
2024-05-13 11:12:44 -0600

    finished sql queries

--- queries.sql
+++ queries.sql
@@ -12,5 +12,16 @@ left join shipper as s
     on o.shipVia = s.id
 where o.id < 10277;                 --This isn't right, I only got 29 orders for some reason.
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
+select productname as "name", quantity
+from orderDetail as o
+join product as p
+    on o.productid = p.id
+where o.orderId = 10251
+order by productname;
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select o.id as orderId, c.companyName, e.lastname as employee
+from 'order' as o
+join customer as c
+    on c.id = o.customerid
+join employee as e
+    on o.employeeid = e.id;
